47caa6333ded230c2722ed8ec628356ca88e4e89
kcalixto <kauacalixtocontato@gmail.com>
2022-09-25 22:42:32 -0300

    new folder structure

---
+++ src/main/java/farmacia/Starter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmacia;
+
+import farmacia.gui.MainScreen;
+
+/**
+ *
+ * @author kcalixto
+ */
+public class Starter {
+    public static void main(String args[]){
+        new MainScreen();
+    }
+}
---
+++ src/main/java/farmacia/logic/stock/objects/Object.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmacia.logic.stock.objects;
+/**
+ *
+ * @author kcalixto
+ */
+public class Object {
+    
+}
