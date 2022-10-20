7a251d87857cf139998122149ebe846d0fd43237
lucasgabrielprojetos <103006053+lucasgabrielprojetos@users.noreply.github.com>
2022-10-19 21:41:16 -0300

    Update Sizes.java

--- src/main/java/farmacia/view/Sizes.java
+++ src/main/java/farmacia/view/Sizes.java
@@ -11,6 +11,5 @@ package farmacia.view;
  */
 public class Sizes {
     public static java.awt.Dimension FRAME_SIZE = new java.awt.Dimension(665,730);
-    public static java.awt.Dimension INTERNAL_FRAME_SIZE = new java.awt.Dimension(650,640);
-    public static java.awt.Dimension PANEL_SIZE = new java.awt.Dimension(640,640);
+    // eh isso memo
 }
